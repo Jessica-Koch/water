122344c58f919bacbf46ea0f8a977d455d23f4f2
Jessica Koch <jessicakoch136@icloud.com>
2016-01-09 00:37:36 -0500

    restructure device object

--- src/js/request.js
+++ src/js/request.js
@@ -71,24 +71,34 @@ var turnOff = function(device){
         xhr.send(); 
     });
 };
-var allOff = function (device) {
-    document.querySelector('#allButton');
-    var selectAllP = new Promise(function(resolve, reject){
+
+function allOff(device) {
+    var selectAllButton = document.querySelector('#allButton');
+    var deviceID = device.id;
+    selectAllButton.addEventListener('click', allOff);
+    new Promise(function(resolve, reject){
+        var json_data = JSON.stringify({id: deviceID});
         var xhr = new XMLHttpRequest(),
-            url = 'https://api.rach.io/1/public/device/stop_water',
-            data = {'id': device.id};
+            url = 'https://api.rach.io/1/public/device/stop_water';
         xhr.open('PUT', url);
         xhr.setRequestHeader('Authorization', 'Bearer ' + sessionStorage.getItem('userKey'));
-        console.log('stop all water request gave response ' + xhr.status + ', data ' + data);
-        if (xhr.status === 204) {
-            var ref = device.zones;
-            var results = [];
-            for (var i = 0, len = ref.length; i < len; i++) {
-                var zone = ref[i];
-                results.push(zone.running = false);
+        xhr.onload = function(){
+            if (xhr.status === 204) {
+                var ref = device.zones;
+                var results = [];
+                for (var i = 0, len = ref.length; i < len; i++) {
+                    var zone = ref[i];
+                    results.push(zone.running = false);
+                }
+                return results;
+            } else {
+                reject(Error(xhr.statusText));
             }
-            return results;
-        }
+        };
+        xhr.onerror = function() {
+            reject(Error('Network Error'));
+        };
+        xhr.send(json_data);
     });
 };
 
@@ -133,22 +143,26 @@ getXMLRequest('person/info', true).then(function(response){
         var userName = document.getElementById('userName');
         var fullName = JSON.parse(response).fullName;
         userName.innerHTML = 'Welcome ' + fullName + ' to your landscaping dashboard!';
-        var deviceId = JSON.parse(response).devices[0].id;
-        localStorage.setItem('deviceId', deviceId);
+        var originalDevice = JSON.parse(response).devices[0];
+        localStorage.setItem('device', JSON.stringify(originalDevice));
+        var device = JSON.parse(localStorage.getItem('device'));
+        return device;
+    }).then(function(device){
+        var deviceID = device.id;
         var deviceContainer = document.getElementById('device-container');
-        var device = localStorage.getItem('deviceId');
-        var unorderedZones = JSON.parse(response).devices[0].zones;
-        var orderedZones = unorderedZones.sort(function(a,b){
-            return a.zoneNumber - b.zoneNumber;
-        });
-        var zoneList = document.createElement('ol');
         var disableAllButton = document.createElement('input');
+        var unorderedZones = device.zones;
+        deviceContainer.appendChild(document.createTextNode(deviceID));
+        deviceContainer.appendChild(disableAllButton);
         disableAllButton.type = 'button';
         disableAllButton.value = 'Disable All';
         disableAllButton.setAttribute('class', 'btn');
         disableAllButton.setAttribute('id', 'allButton');
-        deviceContainer.appendChild(disableAllButton);
-        disableAllButton.onclick = allOff(orderedZones);
+        disableAllButton.onclick = allOff(device);
+        var orderedZones = unorderedZones.sort(function(a,b){
+            return a.zoneNumber - b.zoneNumber;
+        });
+        var zoneList = document.createElement('ol');
         for(var i =0; i < orderedZones.length; i++){
             var zone = document.createElement('section');
             zone.setAttribute('id', orderedZones[i].zoneNumber);
@@ -173,7 +187,7 @@ getXMLRequest('person/info', true).then(function(response){
             zoneList.appendChild(zone);
         }
         document.getElementById('device-container').appendChild(zoneList);
-        // document.getElementById('device-container').appendChild(zoneTable);
+
         
     });
 }).catch(function(error) {
